0d58aee63ebae484b02d7145240da8b5c9caf46b
wiktor <wiktorkujawa1993@gmail.com>
2021-01-22 05:12:01 +0100

    Added server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    credentials: true
+  })
+);
+
+app.use(express.static(path.join(__dirname, './dist')));
+
+app.get('*', (req, res) => { 
+    res.sendFile(path.join(__dirname, './dist/index.html')) 
+}); 
+
+const PORT = process.env.PORT || 4000; 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
