2f9125c3bdaaf426d038203a1a62301ab6cb7967
wiktor <wiktorkujawa1993@gmail.com>
2021-01-22 17:33:46 +0100

    Created README.md and written some tests

--- src/app/layout/layout.component.spec.ts
+++ src/app/layout/layout.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {RouterTestingModule } from '@angular/router/testing';
 import { LayoutComponent } from './layout.component';
 
 describe('LayoutComponent', () => {
@@ -8,6 +8,7 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[ RouterTestingModule],
       declarations: [ LayoutComponent ]
     })
     .compileComponents();
--- src/app/transaction/components/transaction-list/transaction-list.component.spec.ts
+++ src/app/transaction/components/transaction-list/transaction-list.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { StoreModule } from '@ngrx/store';
 import { TransactionListComponent } from './transaction-list.component';
 
 describe('TransactionListComponent', () => {
@@ -8,6 +8,9 @@ describe('TransactionListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({})
+      ],
       declarations: [ TransactionListComponent ]
     })
     .compileComponents();
