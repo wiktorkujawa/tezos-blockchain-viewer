8b05fc74e018e1e1fa3c94fe3eb9877bface8c6e
wiktor <wiktorkujawa1993@gmail.com>
2021-01-23 02:21:43 +0100

    Confifured scroll sizing for different devices

--- src/app/transaction/components/transaction-list/transaction-list.component.ts
+++ src/app/transaction/components/transaction-list/transaction-list.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { ChangeDetectionStrategy, Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { select, Store } from '@ngrx/store';
@@ -22,20 +23,43 @@ export class TransactionListComponent implements OnInit {
 
   limit: number = 10;
 
-  scrollposition: number = 0;
+  tableLimit: number = 10;
+  itemSize: number = 50;
 
   transactions: Transaction[] = [];
   constructor(
-    private store: Store<TransactionState>
-    ) { }
+    private store: Store<TransactionState>,
+    private breakpointObserver: BreakpointObserver) {
+      this.breakpointObserver.observe([
+        '(max-width: 450px)',
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ]).subscribe(result => {
+        if (result.matches) {
+          result.breakpoints['(max-width: 450px)']?
+          (this.itemSize = 104,
+            this.tableLimit = 5):
+          result.breakpoints[Breakpoints.XSmall] ?
+            (this.itemSize = 64,
+              this.tableLimit=10):
+            (this.itemSize = 50,
+              this.tableLimit=10);
+          
+        }
+      });
+    }
 
   ngOnInit(): void {
     this.loadNewTransactions();
   }
 
   onScroll(event: any){
-    if(event.srcElement.scrollTop>(this.limit-10)*50-1)
+    if(event.srcElement.scrollTop>(this.limit-this.tableLimit)*this.itemSize)
     {
+      console.log(this.itemSize);
       this.limit++;
       this.store.dispatch(loadTransactions({limit:this.limit}));
       this.transactions$ = this.store.pipe(select(selectTransactions));
